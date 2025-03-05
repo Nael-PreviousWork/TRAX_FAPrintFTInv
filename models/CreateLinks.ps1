ec4fc72be061782f81b0191363fba3fb23f79ada
Mohammed Nael Mahmoud <49475137+M-Nael@users.noreply.github.com>
2025-03-05 15:37:50 +0000

    Fix structure

---
+++ models/CreateLinks.ps1
@@ -0,0 +1,11 @@
+﻿$AOSMetadataPath = "C:\AOSService\PackagesLocalDirectory"
+
+$RepoModelPath = ".\"
+$RepoModelFolders = Get-ChildItem $RepoModelPath -Directory
+foreach ($ModelFolder in $RepoModelFolders)
+{
+	$Target = "$RepoModelPath\$ModelFolder"
+	New-Item -ItemType SymbolicLink -Path "$AOSMetadataPath" -Name "$ModelFolder" -Value "$Target"
+}
+
+Read-Host -Prompt "Press any key to continue"
---
+++ models/DeleteLinks.ps1
@@ -0,0 +1,11 @@
+﻿$AOSMetadataPath = "C:\AOSService\PackagesLocalDirectory"
+
+$RepoModelsPath = ".\"
+$RepoModelFolders = Get-ChildItem $RepoModelsPath -Directory
+foreach ($ModelFolder in $RepoModelFolders)
+{
+	Write-Output "Deleting $AOSMetadataPath\$ModelFolder"
+	cmd /c rmdir "$AOSMetadataPath\$ModelFolder"
+}
+
+Read-Host -Prompt "Press any key to continue"
